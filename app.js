934555d0a8c1692a47eb4791b6dc0d2686b4039e
unknown <aa0246@srmist.edu.in>
2024-01-10 15:53:29 +0530

    first commit

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(cors({
   methods:["GET","POST","PUT","DELETE"],
   credentials:true,
 }))
+
 // mongoose.connect("mongodb://127.0.0.1:27017", {
 //     dbName: "backendapi",
 // }).then(() =>
