985574c4cb2055328631b8391b996ff919301e49
Ankurra2Kashyap <103994858+Ankurra2Kashyap@users.noreply.github.com>
2024-01-11 19:10:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 import express from "express";
-// import mongoose from "mongoose";
-import userRouter from "./routes/user.js"
-import taskRouter from "./routes/task.js"
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
 import cookieParser from "cookie-parser";
-
-import { config } from "dotenv"
+import { config } from "dotenv";
+import { connectDB } from "./data/database.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
 config({
-  path: "./data/config.env"
-})
-
-import { connectDB } from "./data/database.js"
-import { errorMiddleware } from "./middlewares/error.js";
-import cors from "cors"
+  path: "./data/config.env",
+});
 
 connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+// Use cors middleware to handle CORS headers
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 app.use(userRouter);
 app.use(taskRouter);
-app.use(errorMiddleware); //using error middleware
-app.use(cors({
-  origin:[process.env.FRONTEND_URL],
-  methods:["GET","POST","PUT","DELETE"],
-  credentials:true,
-}))
+app.use(errorMiddleware); // using error middleware
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is working on port :${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
+
 // mongoose.connect("mongodb://127.0.0.1:27017", {
 //     dbName: "backendapi",
 // }).then(() =>
